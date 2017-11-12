64c33ff240e4a9339910064daa510f2f98417377
Serhat Cevikel <serhatcevikel@yahoo.com>
2017-11-12 21:40:46 +0300

    commit

--- style_reference.R
+++ style_reference.R
@@ -26,17 +26,23 @@
 # you may briefly explain its intent with a comment line above
 # note that, for complex variable names incorporating multiple words, separate them with an underscore "_", not a dot "."
 # Dot has a special meaning in the context of object oriented programming
+
 largest_prime_factor <- function(number = 1000) # or you can write a comment at the end of the line
+
 # but it is better not the enlarge the reading span of your code: So extend your code vertically, not horizontally
 # it is better to have more lines, but not have longer lines
 # select self explanatory names for your arguments
-# and put spaces after commas separating the arguments, and spaces before and after equal signs (=) that introduce default values for arguments
+# and put spaces after commas separating the arguments,
+# and spaces before and after equal signs (=) that introduce default values for arguments
 # use curly braces to delimit blocks for functions, loops and condititonals
-# it is best to put opening and closing curly braces in separate lines just below the line initiating the block and leave them alone
-# so that you can track the blocks that you open and close easier
+# it is best to put opening and closing curly braces in separate lines just below the line initiating the block
+# and leave them alone so that you can track the blocks that you open and close easier
 # in a well written code, the opening and closing curly braces for a block are on the same indent level
-{ # the curly brace that introduces the outermost block for the function should be at the beginning of the line, with no leading indents
-    
+
+# the curly brace that introduces the outermost block for the function should be at the beginning of the line,
+# with no leading indents
+{
+
 # introduce an indent for each lower level block that you initiate inside a larger block
 # a level of indent should be introduced by a tab, not single spaces
 
@@ -44,15 +50,20 @@ largest_prime_factor <- function(number = 1000) # or you can write a comment at 
     # put a space after control structure identifiers like "for", "while" or "if"
     # select self explanatory variable names that you introduce in your code
     # make sure that they do not coincide with built-in function, operator or constant names in R
-    # and make sure that in each "for" or "while" loop, you introduce a variable name that was not previously used inside the function
-    # and make sure not to refer to global variable: variables outside the scope of your function (e.g. variables that were introduced before you started your function)
+    # and make sure that in each "for" or "while" loop,
+    # you introduce a variable name that was not previously used inside the function
+    # and make sure not to refer to global variable: variables outside the scope of your function
+    # (e.g. variables that were introduced before you started your function)
     # if you have to use those global variables, pass them using arguments of your function
-    for (factorx in 2:sqrt(number)) # for1: inside the comments you can enumerate nested "for" or "while" loops as such: for1, for2, etc, to better track them
+  
+    for (factorx in 2:sqrt(number)) # for1: inside the comments you can enumerate nested "for" or "while" loops as such:
+    # for1, for2, etc, to better track them
     {
 
         # it is better to put an empty line before and after the blocks to delimit them from the rest of the code
         # this makes the code more readable and blocks more identifiable
         # please put a white space before and after operators such as "%%", "==", "&", "|", "!="
+      
         while (number %% factorx == 0) # while1, again inside the comments you can enumerate the while loops
         {
             # put a space before and after assignment operator "<-"
@@ -60,12 +71,16 @@ largest_prime_factor <- function(number = 1000) # or you can write a comment at 
             number <- number / factorx
             
             # again, wrap your code with empty lines for easier reading
+
             if (number == 1) # if1: inside the commands enumarate your conditional statements to track nested ones
+
             # always separate lines after a control struct (for, while, if, etc) delimiting with curly braces
             {
                 # when you want your code to return a value and stop, then use "return" function
                 # print just prints a value, not returns something to be used for subsequent processess or stops the code
+
                 return(factorx)
+
             } # close if1: put a small comment like this for closing braces, so that you better track your code blocks
             # see that closing brace for if1 is at the same indent with the if statement and the opening brace
             # if you don't want your code to do anyting when a condition is FALSE
@@ -84,6 +99,7 @@ largest_prime_factor <- function(number = 1000) # or you can write a comment at 
     # your code should return a single object before you close the last curly brace
     # even if it is not expected to return anything but stop there
     # put return()    
+
     return(number)
 
 } # see the outmost curly brace that delimits the function block is at the beginning of the line, with no leading indents
